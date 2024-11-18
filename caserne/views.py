c12ded7ef5718e17a85c3067d2548c778389b199
worou36 <101939974+worou36@users.noreply.github.com>
2024-11-18 01:56:17 +0100

    Update views.py

--- caserne/views.py
+++ caserne/views.py
@@ -9,11 +9,10 @@ def soldat_list(request):
 
 
 def soldat_detail(request, id_character):
-    # Récupérer le soldat
+    # Récupérer le soldat et l'ancien lieu
     soldat = get_object_or_404(Character, id_character=id_character)
     ancien_lieu = get_object_or_404(Equipement, id_equip=soldat.lieu.id_equip)
 
-    # Instancier le formulaire lié au soldat
     form = MoveForm(request.POST or None, instance=soldat)
     message = ""
 
@@ -27,33 +26,17 @@ def soldat_detail(request, id_character):
         else:
             # Libérer l'ancien lieu si assigné
             if soldat.lieu:
-                # Le lieu déjà assigné au soldat
-
-                # Vérifier et afficher l'état de l'ancien lieu
-                print(
-                    f"Ancien lieu avant mise à jour : {ancien_lieu.id_equip} - Disponibilité : {ancien_lieu.disponibilite}"
-                )
 
                 # Marquer l'ancien lieu comme libre
                 ancien_lieu.disponibilite = "libre"
                 ancien_lieu.save()  # Sauvegarder l'ancien lieu
-
-                # Vérifier et afficher l'état de l'ancien lieu après mise à jour
-                print(
-                    f"Ancien lieu après mise à jour : {ancien_lieu.id_equip} - Disponibilité : {ancien_lieu.disponibilite}"
-                )
 
             # Sauvegarder le formulaire pour mettre à jour le lieu du soldat
             form.save()
 
             # Marquer le nouveau lieu comme occupé
             nouveau_lieu.disponibilite = "occupé"
-            nouveau_lieu.save()  # Sauvegarder immédiatement après la mise à jour de la disponibilité
-
-            # Vérifier l'état du nouveau lieu
-            print(
-                f"Nouveau lieu après mise à jour : {nouveau_lieu.id_equip} - Disponibilité : {nouveau_lieu.disponibilite}"
-            )
+            nouveau_lieu.save()  
 
             # Rediriger après les modifications
             return redirect("soldat_detail", id_character=soldat.id_character)
